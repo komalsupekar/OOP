4c63bbf6b4e7b3f3ca689caf13a4347207f6fcd0
Komal Bapurao Supekar <55024646+komalsupekar@users.noreply.github.com>
2019-10-20 17:47:01 +0530

    Update and rename README.md to file.cpp

---
+++ file.cpp
@@ -0,0 +1,49 @@
+//file IO cpp
+
+#include <cstdlib>
+ #include <fstream>
+ #include <iostream>
+using namespace std;
+
+int main ()
+ {
+char name[80];
+cout<<"Enter name of file to create\n";
+cin>>name;
+
+ofstream fout;
+
+fout.open(name);
+if(!fout)
+ {
+cout<<"Error opening file\n";
+exit(1);
+ }
+cout<< "Writing to the file" <<endl;
+cout<< "Enter contents for file end with ctrl+D";
+string data;
+while(getline(cin,data))
+ {
+if(data == "^D")
+break;
+fout<< data <<endl;
+ }
+fout.close();
+
+ifstream fin;
+
+fin.open(name);
+if(!fin)
+ {
+cout<<"Error opening file\n";
+exit(1);
+ }
+cout<< "Reading from the file" <<endl;
+while(fin)
+ {
+getline(fin,data);
+cout<< data <<endl;
+ }
+fin.close();
+return 0;
+ }
